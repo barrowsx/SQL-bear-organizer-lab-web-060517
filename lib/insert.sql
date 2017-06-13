b5c9d46b617181ab4fd508a5b5a8b53e716a41e7
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-06-13 11:47:31 -0400

    BEARly got it

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 4728, 'M', 'Burgundy', 'Kerfuffled', 1),
+("Rowdy", -5, 'F', 'Turquoise', 'Surprisingly NOT Rowdy', 0),
+("Tabitha", 10, "M", 'Lavender', 'Lackadaisical', 0),
+("Sergeant Brown", 7890707, 'M', 'Blanco', 'Whimsical', 1),
+("Melissa", 420, 'F', 'Orange', 'Tired af', 1),
+("Grinch", 2, 'M', 'Green', 'Contemplating whether fish have feelings', 1),
+("Wendy", 7000, 'F', 'Red', "Craving delicious Wendy's chicken nuggets", 1),
+(NULL, 0, 'M', 'Red with the blood of his enemies', 'Bloodthirsty', 1);
